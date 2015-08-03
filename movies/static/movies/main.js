10583ec707a32f732f806dfec55aa2d72229915f
JeffreyTerry <jterry94@gmail.com>
2015-08-03 02:59:43 -0500

    added movies/static and changed location of bower files

---
+++ movies/static/movies/main.js
@@ -0,0 +1,56 @@
+$(function () {
+    var $table = $('#table'),
+    $remove = $('#remove'),
+    selections = [];
+
+    $table.bootstrapTable();
+    $(window).resize(function () {
+        $table.bootstrapTable('resetView');
+    });
+});
+
+function detailFormatter(index, row) {
+    var html = [];
+    $.each(row, function (key, value) {
+        html.push('<p><b>' + key + ':</b> ' + value + '</p>');
+    });
+    return html.join('');
+}
+
+function rowStyle(row, index) {
+    return {
+        classes: row['bootstrap_color']
+    };
+}
+
+function titleColumnFormatter(value, row, index) {
+    return '<a href="' + row['imdb_url'] + '" target="_blank" title="' + row['plot'] + '">' + value + '</a>';
+}
+
+function dateSorter(first, second) {
+    if (first && !second) {
+        return -1;
+    } else if (!first && second) {
+        return 1;
+    } else if (!first && !second) {
+        return 0;
+    } else {
+        first = moment(first, 'MM-DD-YYYY');
+        second = moment(second, 'MM-DD-YYYY');
+        return first - second;
+    }
+}
+
+function grossSorter(first, second) {
+    if (first && !second) {
+        return 1;
+    } else if (!first && second) {
+        return -1;
+    } else if (!first && !second) {
+        return 0;
+    } else {
+        first = first.replace(/[$€,]/g, '');
+        second = second.replace(/[$€,]/g, '');
+        return parseInt(first, 10) - parseInt(second, 10);
+    }
+}
